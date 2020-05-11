e97c3869e455fd3499c57da6213065dd6c4c6309
@mgy-webchat <984265486@qq.com>
2020-05-11 17:11:37 +0800

    前端界面从数据库获取数据赋值，页面间跳转存在问题

--- src/main/java/com/boot/controller/pagesController.java
+++ src/main/java/com/boot/controller/pagesController.java
@@ -4,30 +4,38 @@ package com.boot.controller;
 import com.boot.service.tUserService;
 import com.boot.damain.tUser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@RestController
+@Controller
+@CrossOrigin(origins = "*",maxAge = 3600)//解决跨域问题
 public class pagesController {
 
     @Autowired
     private tUserService userService;
 
-    @RequestMapping("/hello")
+    @RequestMapping("/index")
     public String test(){
-        return "Hello Word!";
+        return "index";
+    }
+
+    @RequestMapping("/testPage")
+    public String test1(){
+        return "pages/userInfo";
     }
 
     @RequestMapping("/user")
+    @ResponseBody
     public Map<String ,Object> getUserData(){
         HashMap<String , Object> map = new HashMap<>();
         List<tUser> listUser = userService.selectListUser();
-        map.put("listUser",listUser);
+        map.put("code","0");
+        map.put("count",listUser.size());
+        map.put("data",listUser);
         return map;
     }
 
